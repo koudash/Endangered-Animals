a2149f0a6088af650a701b86d50ac85f25f35a3a
Lei <koudash@gmail.com>
2019-05-12 13:19:52 -0500

    app done. Instead of generating .json file, display data in new tab

--- notebooks/app/app.py
+++ notebooks/app/app.py
@@ -1,11 +1,7 @@
 # flask toolkits
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, jsonify
 from flask_pymongo import PyMongo
 import numpy as np
-import json
-import os
-
-
 
 # Create app and pass __name__
 app = Flask(__name__)
@@ -49,40 +45,6 @@ def name_request():
         nec = q_facts['Native_Extant_Cntry']
     except:
         nec = []
-
-    # Data query and retrival from "temp_by_cntry" Collection
-    list_tavg = []
-    list_tavg_u = []
-    list_date = []
-    cntry_temp = []
-
-    # List to store country names for both native extant resident or native extant   
-    cntry_list = nerc
-    [cntry_list.append(cntry) for cntry in nec if cntry not in cntry_list]
-    
-    # Loop through "cntry_list"
-    for cntry in cntry_list:
-        q_temp = mongo.db.temp_by_cntry.find({"Cntry": cntry})
-
-        # Loop through each document for iterated country in "cntry_list"
-        for case in q_temp:
-            list_tavg.append(case['Avg Temp'])
-            list_tavg_u.append(case['Avg Temp Uncertainty'])
-            list_date.append(case['Date'])
-
-        # Data structure of final output is list of dictionaries  
-        cntry_temp.append({cntry: [{'Avg_Temp': list_tavg}, {'Avg_Temp_Uncertainty': list_tavg_u},\
-            {'Date': list_date}]})
-
-    # Remove existing "cntry_temp_by_animal.json" before saving new requested data
-    try:
-        os.remove('./output/cntry_temp_by_animal.json')
-    except FileNotFoundError:
-        pass
-    
-    # Save "cntry_temp" as .json file for downloading
-    with open ('./output/cntry_temp_by_animal.json', 'w') as json_file:
-        json.dump(cntry_temp, json_file)
 
     # Special character appears in "animal"
     if animal == "Galapagos Penguin":
@@ -140,13 +102,51 @@ def name_request():
     # Clear intermediate query Collection
     mongo.db.query_im.drop()
 
-    # Input queried data into "query_im" Collection with the key of "case_completed"
-    q_input ={}
-    q_input["case_completed"] = query_data    
-    mongo.db.query_im.insert_one(q_input) 
+    # Input queried data into "query_im" Collection with the key of "case_completed"  
+    mongo.db.query_im.insert_one(query_data) 
 
     # Redirect to "/" route after data entry in MongoDB 
     return redirect("/", code=302)
+# >>>>>>>>>>> I AM A ROUTE SEPARATOR <<<<<<<<<<< #
+
+# >>> ROUTE 3 <<< #
+# Define actions for "scrape" route
+@app.route('/api/temperature')
+def api():
+    
+    # Retrieve document in "query_im" Collection
+    query_data = mongo.db.query_im.find_one()
+  
+    # Retrieve name and extant countries of target animal
+    animal = query_data['Common_Name']
+    nerc = query_data['NERC']
+    nec = query_data['NEC']
+
+    # Data query and retrival from "temp_by_cntry" Collection
+    list_tavg = []
+    list_tavg_u = []
+    list_date = []
+    cntry_temp = []
+
+    # List to store country names for both native extant resident or native extant   
+    cntry_list = nerc
+    [cntry_list.append(cntry) for cntry in nec if cntry not in cntry_list]
+
+    # Loop through "cntry_list"
+    for cntry in cntry_list:
+        q_temp = mongo.db.temp_by_cntry.find({"Cntry": cntry})
+
+        # Loop through each document for iterated country in "cntry_list"
+        for case in q_temp:
+            list_tavg.append(case['Avg Temp'])
+            list_tavg_u.append(case['Avg Temp Uncertainty'])
+            list_date.append(case['Date'])
+
+        # Data structure of final output is list of dictionaries  
+        cntry_temp.append({cntry: [{'Avg_Temp': list_tavg}, {'Avg_Temp_Uncertainty': list_tavg_u},\
+            {'Date': list_date}]})
+   
+    return jsonify(cntry_temp)
 # >>>>>>>>>>> I AM A ROUTE SEPARATOR <<<<<<<<<<< #
 
 # Execute the script
